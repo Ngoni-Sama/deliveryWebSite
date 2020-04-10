9f50ec63c1f08d440878571a20e824a71be167f0
marccdb <marcio.bianco@gmail.com>
2020-04-09 23:22:47 -0300

    added first layout for website

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
-const app = expres();
+const app = express();
+
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function{
+    console.log("Server listning on port 3000");
+    
+})
